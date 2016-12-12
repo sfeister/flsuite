d92ae0d94c4f2165c8497a970303fe689911bba3
Scott Feister <sfeister@gmail.com>
2016-12-12 13:04:31 -0600

    Testing out TRIM paths, etc.

---
+++ tests/hello2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+"""
+hello2.py: Dead simple hello world test. Trying out ipython in Spyder3.0.2
+
+Created by Scott Feister on Wed Dec 07 11:21:31 2016
+"""
+
+if __name__ == "__main__":
+    print("Hello, world")
