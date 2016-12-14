0a5a21768816c1a4c3269104be7b7470bd16ad77
Scott Feister <sfeister@gmail.com>
2016-12-14 17:20:26 +0000

    Filename issue

--- oneoff/nifpi1.py
+++ oneoff/nifpi1.py
@@ -9,8 +9,8 @@ Created by Scott Feister on Wed Dec 14 10:50:50 2016
 import flsuite.PI as PI
 
 if __name__ == "__main__":
-    PIdir1 = r"/projects/CosmicLaser/tzeferac/NIF/TDYNO_PROTONS/PR_SPECTRUM/133 kJ"
-    PIdir2 = r"/projects/CosmicLaser/tzeferac/NIF/TDYNO_PROTONS/PR_SPECTRUM/200 kJ"
+    PIdir1 = r"/projects/CosmicLaser/tzeferac/NIF/TDYNO_PROTONS/PR_SPECTRUM/133kJ"
+    PIdir2 = r"/projects/CosmicLaser/tzeferac/NIF/TDYNO_PROTONS/PR_SPECTRUM/200kJ"
     outdir1 = r"/home/sfeister/myouts/NIF_comparisons/ProtonSpectrum/133 kJ 22_5 ns"
     outdir2 = r"/home/sfeister/myouts/NIF_comparisons/ProtonSpectrum/200 kJ 20_0 ns"
 
