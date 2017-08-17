77dd15b23778029d79cd57827f37c5baba7c938a
Scott Feister <sfeister@gmail.com>
2017-08-17 22:03:46 +0000

    Updated yt with a z-splitting function

--- flsuite/flyt/flyt.py
+++ flsuite/flyt/flyt.py
@@ -122,5 +122,38 @@ def fatline2(ds, rad_mm, flds=['density'], npts=1000):
         
     return zgv, fldgv
 
+def zsplits(ds, flds=['density'], npts=21):
+    """Return a lineout of a yt dataset, along z, through X=Y=0 but averaged over X,Y within a cube edge length
+    
+    Splits region into a series of cubes and takes their averages
+    
+    Expects multiple fields; returns dict of field grid vectors.
+    Also, outputs YT arrays (keep units)
+    """
+    # Define the centers
+    zmin = ds.domain_left_edge[2]
+    zmax = ds.domain_right_edge[2]
+    
+    dz = (zmax - zmin)/npts
+    zgv = np.linspace(zmin + dz, zmax - dz, npts)
+    fldgv = {}
+    
+    for i in range(npts):
+        # For each zvalue, define a cube centered on (X, Y, Z) = [0, 0, zcent] and take its average
+        cent = ds.arr([ds.domain_center[0].v, ds.domain_center[1].v, zgv[i].v], 'code_length') # Typically, centered at (0, 0, zcent)
+        le = ds.arr([ds.domain_left_edge[0].v, ds.domain_left_edge[1].v, (zgv[i] - dz/2.0).v], 'code_length') # Sub-region left edge
+        re = ds.arr([ds.domain_right_edge[0].v, ds.domain_right_edge[1].v, (zgv[i] + dz/2.0).v], 'code_length') # Sub-region right edge
+        reg = ds.region(cent, le, re) # Sub-region rectangular prism
+                
+        for fld in flds:
+            myval = reg.mean(fld)
+            if i < 1: # First iteration, allocate arrays
+                fldgv[fld] = ds.arr(np.zeros([npts]), myval.units)
+            fldgv[fld][i] = myval
+            reg.clear_data() # Free up memory after each field; probably good practice given high-res 3D datasets
+        
+    return zgv, fldgv
+
+
 if __name__ == "__main__":
     pass
