3b1e2864e1742b32791359e51737bc0432890033
Scott Feister <sfeister@gmail.com>
2018-07-25 15:13:43 -0500

    Added functions "get_simdata" and "get_cube" to FLASH yt tools (flyt.py)

--- flsuite/flyt/flyt.py
+++ flsuite/flyt/flyt.py
@@ -8,6 +8,120 @@ Created by Scott Feister on Wed Oct 05 14:51:08 2016
 
 import numpy as np
 
+def get_simdata(ds, lev=None, smoothed=False):
+    """ Extract a yt covering grid, covering the domain of the entire FLASH simulation
+    
+    Gives access to underlying data (at fixed resolution) from a FLASH dataset.
+    Works in 1D, 2D, or 3D.
+        
+    Uses yt's "covering grid" method to ensure losslessness.    
+    Replaces get_magnetic_field. This will take the AMR Level, and data set,
+     and it will return a covering grid in the shape
+     of a cube in order to do data analysis.
+
+    Inputs:
+        ds:         YT dataset, the output of one timestep e.g. "yt.load(...)"
+        lev:        int, level of refinement to access, where 0 is no refinement
+                        (yt level 0 matches FLASH lrefine=1)
+                        Default value, None, gives maximum available refinement level.
+    Outputs:
+        cg:         YT covering grid, for the volume of the cube at desired refinement
+    
+    
+    Example usage, 3D Cartesian simulation:
+        ds = yt.load('my/folder/mysim_hdf5_plt_cnt_0010')
+        # Get a cube out at the yt level 3 of simulation refinement (FLASH lrefine=4)
+        cg = get_simdata(ds, lev=3)
+        [xmin, ymin, zmin] = cg.left_edge.v  # Lowermost boundaries (out to the cell walls)
+        [xmax, ymax, zmax] = cg.right_edge.v  # Uppermost boundaries (out to the cell walls)
+        Dens = cg['dens'].v # 3D array of cell density values (axis order: x,y,z)
+        
+    Example usage, 2D Cartesian simulation:       
+        ds = yt.load('my/folder/mysim_hdf5_plt_cnt_0010')
+        # Get a rectangle out at the highest possible level of simulation refinement
+        cg = get_simdata(ds)
+        [xmin, ymin] = cg.left_edge[:2].v
+        [xmax, ymax] = cg.right_edge[:2].v
+        Dens = np.squeeze(cg['dens'].v) # 2D array of cell density values (axis order: x,y)
+        
+        fig, ax = plt.subplots()
+        ax.pcolorfast([xmin, xmax], [ymin, ymax], Dens.T)
+        
+    Example usage, 2D Cylindrical simulation:
+        ds = yt.load('my/folder/mysim_hdf5_plt_cnt_0010')
+        # Get a rectangle out at the highest possible level of simulation refinement
+        cg = get_simdata(ds)
+        [rmin, zmin] = cg.left_edge[:2].v
+        [rmax, zmax] = cg.right_edge[:2].v
+        Dens = np.squeeze(cg['dens'].v) # 2D array of cell density values  (axis order: r,z)
+        
+        fig, ax = plt.subplots()
+        ax.pcolorfast([rmin, rmax], [zmin, zmax], Dens.T)
+        
+    Based on example at http://yt-project.org/doc/examining/low_level_inspection.html#examining-grid-data-in-a-fixed-resolution-array
+    Documented, with examples, by J.T. Laune and Scott Feister. Updated July 2018.
+    """
+    
+    if lev is None:
+        lev = ds.max_level
+    
+    if ds.dimensionality == 1:        
+        dims = ds.domain_dimensions * 2**np.array([lev, 0, 0])
+    elif ds.dimensionality == 2:
+        dims = ds.domain_dimensions * 2**np.array([lev, lev, 0])
+    else:
+        dims = ds.domain_dimensions * 2**lev
+            
+    cg = ds.covering_grid(level=lev, left_edge=ds.domain_left_edge, dims=dims)
+    
+    return cg
+
+def get_cube(ds, edge_um=1000, lev=0, center_yt=None):
+    """
+    Extract a cube of raw data from 3D FLASH yt dataset.
+        
+    Uses yt's "covering grid" method to ensure losslessness.    
+    Replaces get_magnetic_field. This will take the center, edge length,
+     AMR Level, and data set, and it will return a covering grid in the shape
+     of a cube in order to do data analysis.
+
+    Inputs:
+        ds:         YT dataset
+        edge_um:    float, cube edge length in microns
+        lev:        int, level of refinement to access, where 0 is no refinement (lrefine=1)
+        center_yt:  YT array, center position of the cube. If None, domain center is used
+    Outputs:
+        cg:         YT covering grid, for the volume of the cube at desired refinement
+    
+    Example usage:
+        ds = yt.load('my/folder/mydataset0010')
+        # Define cube of density values, offset from domain center in X by 5 microns
+        cg = get_cube(ds, edge_um=500, lev=4, center_yt=ds.domain_center + ds.arr([5,0,0],'um')
+        X = cg['x'] # 3D array of cell X values
+        Y = cg['y'] # 3D array of cell Y values
+        Z = cg['z'] # 3D array of cell Z values
+        Dens = cg['dens'] # 3D array of cell density values
+    
+    Documented, with examples, by J.T. Laune and Scott Feister. Updated July 2018.
+    """
+    
+    # Clean up/ check inputs
+    if center_yt is None: # Set cube center to domain center, if not provided
+        center_yt = ds.domain_center
+    elif not isinstance(center_yt, yt.units.yt_array.YTArray): # Verify input center_yt is a YT Array, e.g. not just a NumPy array
+        raise TypeError("Input 'center_yt' must be a YT Array (of type yt.units.yt_array.YTArray). E.g. try center_yt = ds.arr([0,0,3],'um')")
+     
+    # Extract covering-grid cube with given center and edge length
+    le = center_yt - ds.arr([edge_um/2, edge_um/2, edge_um/2], 'um') # YT left edge for cube
+    re = center_yt + ds.arr([edge_um/2, edge_um/2, edge_um/2], 'um') # YT right edge for cube
+
+    cellwid0 = ds.domain_width/ds.domain_dimensions # Cell width in YT units, at level=0 refinement (lrefine=1)
+    
+    dims = np.array((re - le)/cellwid0) * 2**lev + 1 # Desired array dimensions ([int, int, int]) of covering grid depends on cell resolution
+    cg = ds.covering_grid(level=lev, left_edge=le, dims=dims) # Covering grid cube
+    
+    return cg
+
 # Would like to get this to taking multiple fields, methods, dimensions. Like to be able to lineout other than center as well
 def lineout(ds, field='density', method='ray', npts=1000, axis=0):
     """Return a lineout of a yt dataset, along z, through X=Y=0
